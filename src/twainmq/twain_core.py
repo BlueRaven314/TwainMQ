7f22a4a56146457a7b272171b9a9e12de929693a
Me <Me@home>
2026-06-12 23:19:53 +0100

    multi poll function for multiple consumers

--- src/twainmq/twain_core.py
+++ src/twainmq/twain_core.py
@@ -508,7 +508,8 @@ class TwainMQConsumer(TwainMQBase):
             pass
 
     def heartbeat(self):
-        """Must be called periodically if using a consumer group otherwise you might leave the consumer group"""
+        """Must be called periodically if using a consumer group otherwise you might leave the consumer group.
+        Calling `poll` will call heartbeat first"""
         if self._group is not None:
             while msg := self._group_consumer.poll():
                 self._process_group_msg(msg)
@@ -778,6 +779,24 @@ def partition_hash64(x, partitions) -> int:
     x = x ^ (x >> 31)
     return x % partitions
 
+def multi_poll(consumers, offset=None):
+    """Helper function to poll evenly from multiple consumers.  This is useful because with TwainMQ a consumer can only subscribe to one topic, 
+    so when you need to consume from multiple topics you need multiple consumers.
+    
+    Provide a list of consumers, and an optional offset, and this will poll the consumers in turn until one returns a message.  It will return the index
+    of the consumer that returned the message.  The offset passed in will always be the index of last consumer to be called.  If you store the index from the previous
+    call and pass it back in as the offset you will get a round robin calling sequence. 
+    
+    If offset is not specified (or set to None) then a random offset is chosen, which means that topics will be polled in a random order each time it is called."""
+    n = len(consumers)
+    if offset is None:
+        offset = random.randrange(n)    
+    for c_offset in range(n):
+        consumer_to_poll = consumers[(c_offset + offset + 1) % n]
+        msg = consumer_to_poll.poll()
+        if msg is not None:
+            return ((c_offset + offset + 1) % n, msg)
+    
 class TestBase85Encoding(unittest.TestCase):
     def test_round_trip_small_numbers(self):
         for n in [0, 1, 42, 255, 256, 12345]:
